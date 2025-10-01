f494f7552c9b930498f6222fb4526efb8581bcf6
lordraghul <lyeswtf@gmail.com>
2025-10-01 15:52:51 +0200

    rename domain to lordraghul.com

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true } ,
   app: {
-    baseURL: '/linktree/',
+    baseURL: '/',
     head: {
       title: 'lordraghul - Linktree',
       meta: [
